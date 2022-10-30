41771b1dd9f1a86b5ee31e506a87cf15b5c51cbc
CaioG <116986699+CaioGLima@users.noreply.github.com>
2022-10-30 20:55:10 -0300

    Update menu.js

--- Js/menu.js
+++ Js/menu.js
@@ -2,7 +2,7 @@ let menuJson=[
   {
     id: 1,
     name: "Burrito de Carne",
-    img: "..\Comidons\imagens\burritoCarne.jpg",
+    img: "imagens\burritoCarne.jpg",
     price: [10.00, 12.50, 15.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Burrito recheado com carne"
@@ -10,7 +10,7 @@ let menuJson=[
   {
     id: 2,
     name: "Burrito de Frango",
-    img: "../imagens/burritoF.jpg",
+    img: "imagens/burritoF.jpg",
     price: [10.00, 12.50, 15.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Burrito recheado com Frango e legumes"
@@ -18,7 +18,7 @@ let menuJson=[
   {
     id: 3,
     name: "Burrito Vegano",
-    img: "../imagens/burritovegan.jpg",
+    img: "imagens/burritovegan.jpg",
     price: [11.00, 13.50, 16.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Burrito recheado com legumes e vegetais"
@@ -26,7 +26,7 @@ let menuJson=[
   {
     id: 4,
     name: "Taco Completo",
-    img: "../imagens/taco2.jpg",
+    img: "imagens/taco2.jpg",
     price: [10.00, 12.50, 15.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Taco recheado com carne, vegetais e queijo"
@@ -34,7 +34,7 @@ let menuJson=[
   {
     id: 5,
     name: "Taco de Feijão",
-    img: "../imagens/TF.jpg",
+    img: "imagens/TF.jpg",
     price: [10.00, 12.50, 15.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Taco recheado com carne, feijão e chili"
@@ -42,7 +42,7 @@ let menuJson=[
   {
     id: 6,
     name: "Taco Vegano",
-    img: "../imagens/TacoV.jpg",
+    img: "imagens/TacoV.jpg",
     price: [11.00, 13.50, 16.00],
     sizes: ['15.00cm', '20.00cm', '25.00cm'],
     description: "Taco recheado com vegetais e legumes"
@@ -50,7 +50,7 @@ let menuJson=[
   {
     id: 7,
     name: "Agua Mineral",
-    img: "../imagens/agua.jpg",
+    img: "imagens/agua.jpg",
     price: [3.50, 5.00, 10,00],
     sizes: ['355mL', '500mL', '1L'],
     description: "Agua mineral sem ou com gás"
@@ -58,7 +58,7 @@ let menuJson=[
   {
     id: 8,
     name: "Refrigerante",
-    img: "../imagens/Refri.jpg",
+    img: "imagens/Refri.jpg",
     price: [5.00, 8.00, 12.00],
     sizes: ['355mL', '500mL', '1L'],
     description: "Refrigerante lata"
@@ -66,7 +66,7 @@ let menuJson=[
   {
     id: 9,
     name: "Suco Natural",
-    img: "../imagens/suco.jpg",
+    img: "imagens/suco.jpg",
     price: [5.00, 8.00, 12.00],
     sizes: ['355mL', '500mL', '1L'],
     description: "Suco Natural One"
